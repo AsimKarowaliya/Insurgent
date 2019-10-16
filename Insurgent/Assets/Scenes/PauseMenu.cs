75fb8bc8709840418e97f6c45b3a737eb13b634e
catzalez <catzalez@users.noreply.github.com>
2019-10-15 19:47:30 -0500

    adding pause button

--- Insurgent/Assets/Scenes/PauseMenu.cs
+++ Insurgent/Assets/Scenes/PauseMenu.cs
@@ -17,7 +17,8 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetButtonDown("Escape"))
+            //if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
