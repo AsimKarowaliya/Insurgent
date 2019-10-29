ae824cfcd3caeed10329bd276fb5f4cd3741d675
smaredia11 <smaredia11@users.noreply.github.com>
2019-10-29 09:48:43 -0500

    midterm-commit

---
+++ Insurgent/Assets/deathzone_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathzone_script : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            HealthSystem HS = collision.GetComponent<HealthSystem>();
+            HS.playerHealth = 0;
+        }
+    }
+}
